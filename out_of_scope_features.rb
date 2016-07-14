d6306321624b30441d06afc36a66b472e2363e2b
Ivan Ushakov <ivan.ushakov@goeuro.com>
2016-07-14 09:07:14 +0300

    added-more-ARGV
    Added more ARGVs to specify before running a script:
    - extension
    - word

--- out_of_scope_features.rb
+++ out_of_scope_features.rb
@@ -1,12 +1,21 @@
-AGRV = "mydir" # ruby out_of_scope_features.rb /PATH_TO_PROJECT/
+### ruby out_of_scope_features.rb mydir extension word
+### example: ruby out_of_scope_features.rb /Users/admin/calabash-android/ feature London
+### AGRVs:
+### mydir - path to folder that should be checked
+### extension - extension of files that should be checked
+### word - word that should be checked
 
-Dir.chdir( ARGV[0] )
-%[cd ARGV[0]]
+mydir = ARGV[0]
+extension = ARGV[1]
+word = ARGV[2]
 
-Dir.glob("**/*.feature") {|file|
+Dir.chdir( mydir )
+%[cd mydir]
+
+Dir.glob("**/*.#{extension}") {|file|
   File.open file do |f|
     f.any? do |line|
-      if line.include?('@out_of_scope')
+      if line.include?(word)
         puts file
         break
       end
